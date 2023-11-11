6a2e0c3fe4b955788d0598ab30148b4e7ff25068
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-11-11 23:36:39 +0900

    :sparkles: yarn shadcn-ui init

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-import { headers } from "next/headers";
-import "server-only";
-
-export const getUrl = () => {
-  const headerList = headers();
-  const url = headerList.get("x-url");
-  return url;
-};
